6545024973a80e02eb2d0526b3f333f0dca4328a
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2023-06-24 22:03:15 +0700

    Revert "Add CONFIG_KSU"
    This reverts commit 0e79c4bf27d14e5d575e2a5472593716b216b8a8.

--- fs/exec.c
+++ fs/exec.c
@@ -1988,10 +1988,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
-
-#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
-#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
@@ -2003,10 +2000,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-#ifdef CONFIG_KSU
+	
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
